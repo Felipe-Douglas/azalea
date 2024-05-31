ed3de4f20481e82bba4f515f17c22c75fdffc724
Filipe Douglas <felipe.douglas.inf1@gmail.com>
2024-05-31 14:24:16 -0300

    adicionado uma nova rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProcessesComponent } from './core/layouts/processes/processes.component';
 import { ContractsComponent } from './core/layouts/contracts/contracts.component';
 import { CalendarComponent } from './core/layouts/calendar/calendar.component';
+import { HomeComponent } from './core/layouts/home/home.component';
 
 
 export const routes: Routes = [
+    {path: "home", component: HomeComponent},
     {path: "processs", component: ProcessesComponent},
     {path: "contratos", component: ContractsComponent},
     {path: "calendar", component: CalendarComponent}
